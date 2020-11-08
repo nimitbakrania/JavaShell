df34cab702533b7910eb2d0fa098f9e9f7b590f6
Anirudh Lakra <55798132+anirudh1666@users.noreply.github.com>
2020-11-08 07:36:18 +0000

    Update Factory.java

--- src/main/java/uk/ac/ucl/jsh/files/Factory.java
+++ src/main/java/uk/ac/ucl/jsh/files/Factory.java
@@ -8,35 +8,35 @@ public class Factory {
 
     public Factory() { }
 
-    public Visitable mkApplication(InputStream in, OutputStream out, String application, ArrayList<String> appArgs) {
+    public Visitable mkApplication(InputStream in, OutputStream out, String currentDirectory, String application, ArrayList<String> appArgs) {
         Visitable app;
         switch (application) {
             case "cd":
-                app = new Visitable.Cd(in, out, appArgs);
+                app = new Visitable.Cd(in, out, currentDirectory, appArgs);
                 break;
             case "pwd":
-                app = new Visitable.Pwd(in, out, appArgs);
+                app = new Visitable.Pwd(in, out, currentDirectory, appArgs);
                 break;
             case "find":
-                app = new Visitable.Find(in, out, appArgs);
+                app = new Visitable.Find(in, out, currentDirectory, appArgs);
                 break;
             case "ls":
-                app = new Visitable.Ls(in, out, appArgs);
+                app = new Visitable.Ls(in, out, currentDirectory, appArgs);
                 break;
             case "cat":
-                app = new Visitable.Cat(in, out, appArgs);
+                app = new Visitable.Cat(in, out, currentDirectory, appArgs);
                 break;
             case "echo":
-                app = new Visitable.Echo(in, out, appArgs);
+                app = new Visitable.Echo(in, out, currentDirectory, appArgs);
                 break;
             case "head":
-                app = new Visitable.Head(in, out, appArgs);
+                app = new Visitable.Head(in, out, currentDirectory, appArgs);
                 break;
             case "tail":
-                app = new Visitable.Tail(in, out, appArgs);
+                app = new Visitable.Tail(in, out, currentDirectory, appArgs);
                 break;
             case "grep":
-                app = new Visitable.Grep(in, out, appArgs);
+                app = new Visitable.Grep(in, out, currentDirectory, appArgs);
                 break;
             default:
                 throw new RuntimeException(application + ": unknown application");
