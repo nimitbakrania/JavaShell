172917dcbcab87f81908a8137927a1ea122c77b4
Anirudh Lakra <55798132+anirudh1666@users.noreply.github.com>
2020-11-08 07:28:16 +0000

    Update Call.java

--- src/main/java/uk/ac/ucl/jsh/files/Call.java
+++ src/main/java/uk/ac/ucl/jsh/files/Call.java
@@ -8,7 +8,7 @@ public class Call implements Command {
 
     private visitorApplication visitor = new visitorApplication();
 
-    public void eval(InputStream in, OutputStream out, String app, ArrayList<String> appArgs) {
+    public void eval(InputStream in, OutputStream out, String currentDirectory, String app, ArrayList<String> appArgs) {
 
         Factory factory = new Factory();
         Visitable application = factory.mkApplication(in, out, app, appArgs);
