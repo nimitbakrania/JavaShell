6bc35380de0f76726fa1b6bc7ca258da739cedf4
Anirudh Lakra <55798132+anirudh1666@users.noreply.github.com>
2020-11-23 11:22:00 +0000

    Started tests

--- src/test/java/uk/ac/ucl/jsh/JshTest.java
+++ src/test/java/uk/ac/ucl/jsh/JshTest.java
@@ -8,18 +8,31 @@ import java.io.PipedOutputStream;
 import java.util.Scanner;
 
 public class JshTest {
+
+    private PipedInputStream in = new PipedInputStream();
+    private PipedOutputStream out = new PipedOutputStream(in);
+
     public JshTest() {
     }
 
     @Test
-    public void testJsh() throws Exception {
-        PipedInputStream in = new PipedInputStream();
-        PipedOutputStream out;
-        out = new PipedOutputStream(in);
-        Jsh.eval("echo foo", out);
+    public void testEchoAppOneArg() throws Exception {
+
+        Jsh.eval("echo foo", this.out);
         Scanner scn = new Scanner(in);
-        assertEquals(scn.next(),"foo");
+        assertEquals(scn.next(), "foo");
         scn.close();
+    }
+
+    @Test
+    public void testEchoAppMultipleArgs() throws Exception {
+
+        
+    }
+
+    @Test
+    public void testJsh() throws Exception {
+        // ANIRHDh - Change this so that its a system test. It tests each application in jsh to make sure they all work.
     }
 
 }
