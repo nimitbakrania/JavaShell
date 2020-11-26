e8d08dbc35d4ab188959963d1c8a676527f8c232
Julian Popovski-Jones <zcabpop@ucl.ac.uk>
2020-11-26 16:43:07 +0400

    Using temp files for find head grep pwd tests

--- src/test/java/uk/ac/ucl/jsh/grepTest.java
+++ src/test/java/uk/ac/ucl/jsh/grepTest.java
@@ -1,38 +1,69 @@
 package uk.ac.ucl.jsh;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 
 public class grepTest {
     // Needs to test 0 args 2 args >2 args, invalid file name, valid
+    @Rule
+    public TemporaryFolder TempFolder = new TemporaryFolder();
+
+    @Before
+    public void EnterTempFolder() throws IOException{
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        Jsh.eval("cd" + TempFolder.getRoot(), out);
+    }
+    //piped
+    //piped
+    //eval cd
+
     @Test
     public void invalidFileName() throws IOException{
+        File tempFile = TempFolder.newFile("Test1.txt");
+        FileWriter tempFileWriter = new FileWriter(tempFile, StandardCharsets.UTF_8);
+        tempFileWriter.write("Hello\nhello world");
+
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out;
         out = new PipedOutputStream(in);
         try{
-            Jsh.eval("grep 'hello world' src\\main\\java\\uk\\ac\\ucl\\jsh\\TestFiles\\abcd.txt", out);
+            Jsh.eval("grep 'hello world' Test.txt", out);
         }
         catch(IOException e){
+            tempFileWriter.close();
             assertEquals("grep: wrong file argument", e.toString());
         }
+        tempFileWriter.close();
     }
 
     @Test
     public void twoArgTest() throws IOException{
+        File tempFile = TempFolder.newFile("Test.txt");
+        FileWriter tempFileWriter = new FileWriter(tempFile, StandardCharsets.UTF_8);
+        tempFileWriter.write("Hello\nhello world hello everybody\nJava");
+
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out;
         out = new PipedOutputStream(in);
-        Jsh.eval("grep 'hello world' src\\main\\java\\uk\\ac\\ucl\\jsh\\TestFiles\\abc.txt", out);
+        Jsh.eval("grep 'hello world' Test.txt", out);
         Scanner scn = new Scanner(in);
         assertEquals(scn.nextLine(), "Nullam dictum felis eu pede mollis pretium. Integer tincidunt. hello world Cras dapibus.");
         scn.close();
+        tempFileWriter.close();
     }
 
     @Test
@@ -50,14 +81,25 @@ public class grepTest {
 
     @Test
     public void manyArgsTest() throws IOException{
+        File tempFile = TempFolder.newFile("Test.txt");
+        FileWriter tempFileWriter = new FileWriter(tempFile, StandardCharsets.UTF_8);
+        tempFileWriter.write("Hello\nhello world hello everybody\nJava");
+        
+        File tempFile2 = TempFolder.newFile("Test2.txt");
+        FileWriter tempFileWriter2 = new FileWriter(tempFile2, StandardCharsets.UTF_8);
+        tempFileWriter2.write("Hello\nhello world tempfile2\nhello world Java");
+
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out;
         out = new PipedOutputStream(in);
-        Jsh.eval("grep 'Nullam dictum felis' 'src\\main\\java\\uk\\ac\\ucl\\jsh\\TestFiles\\abc.txt' 'src\\main\\java\\uk\\ac\\ucl\\jsh\\TestFiles\\a.txt'", out);
+        Jsh.eval("grep 'hello world' Test.txt Test2.txt", out);
         Scanner scn = new Scanner(in);
-        assertEquals(scn.nextLine(), "src\\main\\java\\uk\\ac\\ucl\\jsh\\TestFiles\\abc.txt:Nullam dictum felis eu pede mollis pretium. Integer tincidunt. hello world Cras dapibus.");
-        assertEquals(scn.nextLine(), "src\\main\\java\\uk\\ac\\ucl\\jsh\\TestFiles\\a.txt:Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.");
+        assertEquals(scn.nextLine(), "Test.txt:hello world hello everybody");
+        assertEquals(scn.nextLine(), "Test2.txt:hello world tempfile2");
+        assertEquals(scn.nextLine(), "Test2.txt:hello world Java");
         scn.close();
+        tempFileWriter.close();
+        tempFileWriter2.close();
     }
 
 }
