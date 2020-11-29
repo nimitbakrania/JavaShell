75554243d02aec62d53b8f8c9b64817e9f341a2a
JulianPJ <56226778+JulianPJ@users.noreply.github.com>
2020-11-29 18:11:26 +0400

    Safe and unsafe update

--- src/main/java/uk/ac/ucl/jsh/Factory.java
+++ src/main/java/uk/ac/ucl/jsh/Factory.java
@@ -17,44 +17,45 @@ public class Factory {
         }
         switch (application) {
             case "cd":
-                app = new UnsafeDecorator(new Visitable.Cd(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Cd(in, out, appArgs, currentDirectory), flag);
                 break;
             case "pwd":
-                app = new UnsafeDecorator(new Visitable.Pwd(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Pwd(in, out, appArgs, currentDirectory), flag);
                 break;
             case "ls":
-                app = new UnsafeDecorator(new Visitable.Ls(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Ls(in, out, appArgs, currentDirectory), flag);
                 break;
             case "cat":
-                app = new UnsafeDecorator(new Visitable.Cat(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Cat(in, out, appArgs, currentDirectory), flag);
                 break;
             case "echo":
-                app = new UnsafeDecorator(new Visitable.Echo(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Echo(in, out, appArgs, currentDirectory), flag);
                 break;
             case "head":
-                app = new UnsafeDecorator(new Visitable.Head(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Head(in, out, appArgs, currentDirectory), flag);
                 break;
             case "tail":
-                app = new UnsafeDecorator(new Visitable.Tail(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Tail(in, out, appArgs, currentDirectory), flag);
                 break;
             case "grep":
-                app = new UnsafeDecorator(new Visitable.Grep(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Grep(in, out, appArgs, currentDirectory), flag);
                 break;
             case "cut":
-                app = new UnsafeDecorator(new Visitable.Cut(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Cut(in, out, appArgs, currentDirectory), flag);
                 break;
             case "find":
-                app = new UnsafeDecorator(new Visitable.Find(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Find(in, out, appArgs, currentDirectory), flag);
                 break;
             case "uniq":
-                app = new UnsafeDecorator(new Visitable.Uniq(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Uniq(in, out, appArgs, currentDirectory), flag);
                 break;
             case "sort":
-                app = new UnsafeDecorator(new Visitable.Uniq(in, out, appArgs, currentDirectory));
+                app = new UnsafeDecorator(new Visitable.Sort(in, out, appArgs, currentDirectory), flag);
                 break;
             default:
                 throw new RuntimeException(application + ": unknown application");
             }
 		return app;
     }
+    
 }
