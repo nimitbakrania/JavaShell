73fc3bacf94087c30c0f1e1157c3a123332127c8
JulianPJ <56226778+JulianPJ@users.noreply.github.com>
2020-11-29 18:12:02 +0400

    Unsafe and safe decorators

--- src/main/java/uk/ac/ucl/jsh/UnsafeDecorator.java
+++ src/main/java/uk/ac/ucl/jsh/UnsafeDecorator.java
@@ -3,10 +3,11 @@ package uk.ac.ucl.jsh;
 // catch all exceptions here and print instead of throwing, allowing the program to continue instead of terminating
 public class UnsafeDecorator {
     private Visitable app;
-
-    public UnsafeDecorator(Visitable application)
+    private boolean unsafeFlag;
+    public UnsafeDecorator(Visitable application, boolean flag)
     {
         this.app = application;
+        this.unsafeFlag = flag;
     }
 
     public void accept(baseVisitor visitor) { 
@@ -15,7 +16,12 @@ public class UnsafeDecorator {
         }
         catch(Exception e)
         {
-            System.err.println(e.getMessage());
+            if (unsafeFlag){
+                System.err.println(e.getMessage());
+            }
+            else{
+                throw new RuntimeException(e.toString());
+            }
         }
 
     }
