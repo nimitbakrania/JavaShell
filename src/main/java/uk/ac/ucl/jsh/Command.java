f97e6e48b2d399be7d470c69eb4bcbf976413e3f
Julian Popovski-Jones <zcabpop@ucl.ac.uk>
2020-12-02 16:58:49 +0400

    Command throws IOException

--- src/main/java/uk/ac/ucl/jsh/Command.java
+++ src/main/java/uk/ac/ucl/jsh/Command.java
@@ -1,9 +1,10 @@
 package uk.ac.ucl.jsh;
 
 import java.io.OutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
 public interface Command {
-    public void eval(InputStream cmdline, OutputStream output, String currentDirectory, String app, ArrayList<String> appArgs);
+    public void eval(InputStream cmdline, OutputStream output, String currentDirectory, String app, ArrayList<String> appArgs) throws IOException;
 }
