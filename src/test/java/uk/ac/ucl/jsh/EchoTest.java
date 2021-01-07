0f2bb76615ed1bdbeaecc7894e3cca5baf3503d6
JulianPJ <56226778+JulianPJ@users.noreply.github.com>
2021-01-07 19:22:06 +0400

    Fix EchoTest

--- src/test/java/uk/ac/ucl/jsh/EchoTest.java
+++ src/test/java/uk/ac/ucl/jsh/EchoTest.java
@@ -1,38 +1,49 @@
 package uk.ac.ucl.jsh;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.rules.TemporaryFolder;
 
+import static org.junit.Assert.*;
+import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.util.Scanner;
 
 public class EchoTest {
-    
+    @Rule
+    public TemporaryFolder TempFolder = new TemporaryFolder();
+
+    @Before
+    public void EnterTempFolder() throws IOException{
+        Jsh.setCurrentDirectory(TempFolder.getRoot().toString());
+    }
+
     @Test
-    public void testEchoAppOneArg() throws Exception {
+    public void testEchoAppOneArg() throws IOException {
 
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out = new PipedOutputStream(in);        
-        Jsh.eval("echo foo", out);
+        Jsh.eval("_echo foo", out);
         Scanner scn = new Scanner(in);
         assertEquals("foo", scn.next());
         scn.close();
     }
 
     @Test
-    public void testEchoNoArgs() throws Exception {
+    public void testEchoNoArgs() throws IOException {
 
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out = new PipedOutputStream(in);
-        Jsh.eval("echo", out);
+        Jsh.eval("_echo", out);
         Scanner scan = new Scanner(in);
         assertTrue(scan.nextLine().isEmpty());
         scan.close();
     }
 
     @Test
-    public void testEchoAppMultipleArgs() throws Exception {
+    public void testEchoAppMultipleArgs() throws IOException {
 
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out = new PipedOutputStream(in);    
