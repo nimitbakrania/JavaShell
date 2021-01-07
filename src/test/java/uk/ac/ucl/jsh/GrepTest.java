690c4f8a8969b024dbf536d6284f9427b4e631f6
Julian Popovski-Jones <zcabpop@ucl.ac.uk>
2021-01-07 21:46:24 +0400

    Added Grep unit tests

--- src/test/java/uk/ac/ucl/jsh/GrepTest.java
+++ src/test/java/uk/ac/ucl/jsh/GrepTest.java
@@ -1,6 +1,5 @@
 package uk.ac.ucl.jsh;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,11 +8,13 @@ import org.junit.rules.TemporaryFolder;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -23,58 +24,70 @@ public class GrepTest {
     public TemporaryFolder TempFolder = new TemporaryFolder();
 
     @Before
-    public void EnterTempFolder() throws IOException{
+    public void setUp() throws IOException{
         Jsh.setCurrentDirectory(TempFolder.getRoot().toString());
-    }
 
-    @After
-    public void deleteTempFolder(){
-        TempFolder.delete();
-    }
-
-    @Test
-    public void stdinTest() throws IOException{
-        File tempFile = TempFolder.newFile("Test1.txt");
-        FileWriter w = new FileWriter(tempFile);
-        w.write("Hello Test\nhello world test\n Test Test test \n lorem\nipsum\ndolor");;
-        w.close();
-        PipedInputStream in = new PipedInputStream();
-        PipedOutputStream out;
-        out = new PipedOutputStream(in);
-        Jsh.eval("head -n 4 " + tempFile.getName() + " | grep Test", out);
-        Scanner scn = new Scanner(in);
-        assertEquals("Hello Test", scn.nextLine());
-        assertEquals(" Test Test test ", scn.nextLine());
-        scn.close();
-
-    } 
-
-
-    @Test
-    public void invalidFileName() throws IOException{
-        File tempFile = TempFolder.newFile("Test1.txt");
-        FileWriter tempFileWriter = new FileWriter(tempFile, StandardCharsets.UTF_8);
-        tempFileWriter.write("Hello\nhello world");
-        tempFileWriter.close();
-
-        PipedInputStream in = new PipedInputStream();
-        PipedOutputStream out;
-        out = new PipedOutputStream(in);
-        try{
-            Jsh.eval("_grep 'hello world' Test.txt", out);
-        }
-        catch(IOException e){
-            assertEquals("grep: wrong file argument", e.toString());
-        }
-    }
-
-    @Test
-    public void twoArgTest() throws IOException{
         File tempFile = TempFolder.newFile("Test.txt");
         FileWriter tempFileWriter = new FileWriter(tempFile, StandardCharsets.UTF_8);
         tempFileWriter.write("Hello\nhello world hello everybody\nJava");
         tempFileWriter.close();
 
+        File tempFile2 = TempFolder.newFile("Test2.txt");
+        FileWriter tempFileWriter2 = new FileWriter(tempFile2, StandardCharsets.UTF_8);
+        tempFileWriter2.write("Hello\nhello world tempfile2\nhello world Java");
+        tempFileWriter2.close();
+    }
+
+    @Test
+    public void stdinTest() throws IOException{
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out;
+        out = new PipedOutputStream(in);
+        Jsh.eval("head -n 2 Test.txt | grep world", out);
+        Scanner scn = new Scanner(in);
+        assertEquals("hello world hello everybody", scn.nextLine());
+        scn.close();
+    } 
+    
+    @Test
+    public void stdinUnitTest() throws IOException{
+        FileInputStream stdin = new FileInputStream(new File(TempFolder.getRoot().toString(), "Test.txt"));
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        ArrayList<String> appArgs = new ArrayList<String>();
+        appArgs.add("world");
+        new Visitable.Grep(stdin, out, appArgs).accept(new VisitorApplication());
+        Scanner scn = new Scanner(in);
+        assertEquals("hello world hello everybody", scn.nextLine());
+        scn.close();
+    } 
+
+
+    @Test
+    public void invalidFileName() throws IOException{
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out;
+        out = new PipedOutputStream(in);
+        try{
+            Jsh.eval("_grep 'hello world' Test1.txt", out);
+        }
+        catch(IOException e){
+            assertEquals("grep: wrong file argument", e.toString());
+        }
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void invalidFileNameUnitTest() throws IOException{
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        ArrayList<String> appArgs = new ArrayList<String>();
+        appArgs.add("hello world");
+        appArgs.add("Test1.txt");
+        new Visitable.Grep(null, out, appArgs).accept(new VisitorApplication());
+    } 
+
+    @Test
+    public void twoArgTest() throws IOException{
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out;
         out = new PipedOutputStream(in);
@@ -83,6 +96,20 @@ public class GrepTest {
         assertEquals(scn.nextLine(), "hello world hello everybody");
         scn.close();
     }
+        
+    @Test
+    public void twoArgUnitTest() throws IOException{
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        ArrayList<String> appArgs = new ArrayList<String>();
+        appArgs.add("hello world");
+        appArgs.add("Test.txt");
+        new Visitable.Grep(null, out, appArgs).accept(new VisitorApplication());
+        Scanner scn = new Scanner(in);
+        assertEquals(scn.nextLine(), "hello world hello everybody");
+        scn.close();
+    } 
+
 
     @Test
     public void noArgsTest() throws IOException{ //error one
@@ -96,19 +123,18 @@ public class GrepTest {
             assertEquals("grep: no arguments given", e.toString());
         }
     }
+        
+    @Test(expected = RuntimeException.class)
+    public void noArgsUnitTest() throws IOException{
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        ArrayList<String> appArgs = new ArrayList<String>();
+        new Visitable.Grep(null, out, appArgs).accept(new VisitorApplication());
+    } 
+
 
     @Test
     public void manyArgsTest() throws IOException{
-        File tempFile = TempFolder.newFile("Test.txt");
-        FileWriter tempFileWriter = new FileWriter(tempFile, StandardCharsets.UTF_8);
-        tempFileWriter.write("Hello\nhello world hello everybody\nJava");
-        tempFileWriter.close();
-        
-        File tempFile2 = TempFolder.newFile("Test2.txt");
-        FileWriter tempFileWriter2 = new FileWriter(tempFile2, StandardCharsets.UTF_8);
-        tempFileWriter2.write("Hello\nhello world tempfile2\nhello world Java");
-        tempFileWriter2.close();
-
         PipedInputStream in = new PipedInputStream();
         PipedOutputStream out;
         out = new PipedOutputStream(in);
@@ -119,5 +145,20 @@ public class GrepTest {
         assertEquals(scn.nextLine(), "Test2.txt:hello world Java");
         scn.close();
     }
-
+   
+    @Test
+    public void manyArgsUnitTest() throws IOException{
+        PipedInputStream in = new PipedInputStream();
+        PipedOutputStream out = new PipedOutputStream(in);
+        ArrayList<String> appArgs = new ArrayList<String>();
+        appArgs.add("hello world");
+        appArgs.add("Test.txt");
+        appArgs.add("Test2.txt");
+        new Visitable.Grep(null, out, appArgs).accept(new VisitorApplication());
+        Scanner scn = new Scanner(in);
+        assertEquals(scn.nextLine(), "Test.txt:hello world hello everybody");
+        assertEquals(scn.nextLine(), "Test2.txt:hello world tempfile2");
+        assertEquals(scn.nextLine(), "Test2.txt:hello world Java");
+        scn.close();
+    } 
 }
